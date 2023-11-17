278bca40e9935848d1be791816a8c519ca43a5bc
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-17 10:43:59 +0100

    FEAT 1st milestone

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const eventRouter = require('./routes/events')
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/events', eventRouter);
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+});
